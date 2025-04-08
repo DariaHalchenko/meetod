08445c6224fa430bc1f330f80221cc631cf052f1
DariaHalchenko <154507353+DariaHalchenko@users.noreply.github.com>
2025-04-08 11:34:07 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,27 @@
+#tasks=[]
+#def add_task(task):
+#    return tasks.append(task)
+
+# streamlit
+
+# def main():
+#    print('1-lisada ülesande \n 2-kustuda ülesande \n 3-ülevaadata ülesanded')
+#    userInput=input("Mida sa tahad? ")
+#    if userInput=='1':
+#        tasks=add_task(input("sisesta ülesande: "))
+#    elif userInput =='2':
+#        pass 
+#    elif userInput =='3':
+#        pass 
+#    else:
+#        print("sa sisestasid midagi vale")
+
+import streamlit as st
+if "tasks" not in st.session_state:
+    st.session_state.tasks = []
+st.title("Todo List")    
+
+def add_task():
+    
+
+
